c744b6e1303b96338822e08e43b2c99093ab9e80
Stefan Holzmüller <stefanholzmueller@github>
2016-10-24 20:24:18 +0200

    fix tests

--- test/Main.purs
+++ test/Main.purs
@@ -2,28 +2,38 @@ module Test.Main where
 
 import Prelude
 import Life as L
-import Data.Array (length)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
+import Data.Array (filter, length)
 import Data.Foldable (all, elem)
-import Test.QuickCheck (quickCheck, QC)
+import Test.QuickCheck (assertEquals, quickCheck', quickCheck, QC)
 import Test.QuickCheck.Arbitrary (class Arbitrary)
 import Test.QuickCheck.Gen (chooseInt)
 
-newtype RandomCell = RandomCell L.Cell
-instance arbitraryCell :: Arbitrary RandomCell where
+newtype RandomCoords = RandomCoords L.Coords
+instance arbitraryCoords :: Arbitrary RandomCoords where
   arbitrary = do
               x <- chooseInt 0 99
               y <- chooseInt 0 99
-              pure $ RandomCell $ Cell { x, y }
+              pure $ RandomCoords $ L.Coords { x, y }
 
 main :: forall eff. QC eff Unit
 main = do
-    quickCheck everyCellHasEightNeighbors
-    quickCheck aCellsNeighborHasNeighborsContainingTheCell
+       quickCheck everyCellHasEightNeighbors
+       quickCheck aCellsNeighborHasNeighborsContainingTheCell
+       quickCheck' 1 gliderInFifthGeneration
   where
-    everyCellHasEightNeighbors (RandomCell cell) =
-        length (L.neighbors cell) == 8
-    aCellsNeighborHasNeighborsContainingTheCell (RandomCell cell) =
+    everyCellHasEightNeighbors (RandomCoords coords) =
+        length (L.neighbors coords) == 8
+
+    aCellsNeighborHasNeighborsContainingTheCell (RandomCoords coords) =
         all (_ == true) do
-          n <- L.neighbors cell
+          n <- L.neighbors coords
           let ns = L.neighbors n
-          pure (elem cell ns)
+          pure (elem coords ns)
+
+    gliderInFifthGeneration (RandomCoords _) = assertEquals [] []
+      where
+        cell x y = L.Cell (L.Coords { x: x, y: y }) L.Alive
+        initial = [ cell 2 1, cell 3 2, cell 3 3, cell 2 3, cell 1 3 ]
+        gen4 = unsafePerformEff ((L.gameOfLife initial) >>= L.gameOfLife >>= L.gameOfLife >>= L.gameOfLife)
+        gen5 = unsafePerformEff (L.gameOfLife gen4)
