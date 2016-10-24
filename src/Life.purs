60ea36d4dd41762345dc3a8f79f17211dbf12ff6
Stefan Holzmüller <stefanholzmueller@github>
2016-10-24 08:37:30 +0200

    implemented randomness, but with unsafePerformEff and messy type annotations

--- src/Life.purs
+++ src/Life.purs
@@ -1,14 +1,19 @@
 module Life where
 
 import Prelude
+import Control.Apply (lift2)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random (randomRange, RANDOM)
 import Control.MonadZero (guard)
 import Data.Array (difference, filter, intersect, length, nub)
+import Data.Traversable (sequence, traverse)
 
 newtype Coords = Coords { x :: Int, y :: Int }
 data CellState = Alive | Dead Int | Zombie
 data Cell = Cell Coords CellState
 type CellContext = Int  -- number of alive neighbors
 type Population = Array Cell
+type StateChangeConfig = CellState -> CellContext -> Eff (random :: RANDOM) CellState
 
 instance eqCoords :: Eq Coords
   where eq (Coords c1) (Coords c2) = c1.x == c2.x && c1.y == c2.y
@@ -20,19 +25,20 @@ isAlive (Cell _ _)     = false
 getCoords :: Cell -> Coords
 getCoords (Cell coords _) = coords
 
-gameOfLife :: Population -> Population
+gameOfLife :: Population -> Eff (random :: RANDOM) Population
 gameOfLife = nextGen neighbors stateChange
 
-nextGen :: (Coords -> Array Coords) -> (CellState -> CellContext -> CellState) -> Population -> Population
-nextGen neighborsConfig stateChangeConfig population = (map nextState population) <> offspring
+nextGen :: (Coords -> Array Coords) -> StateChangeConfig -> Population -> Eff (random :: RANDOM) Population
+nextGen neighborsConfig stateChangeConfig population = lift2 append (traverse nextState population) offspring
   where
-    coordsOfAliveCells = map getCoords (filter isAlive population)
-    numberOfAliveNeighbors coords = length (intersect coordsOfAliveCells (neighbors coords))
-    nextState (Cell coords state) = Cell coords (stateChangeConfig state (numberOfAliveNeighbors coords))
-    populationCoords = map getCoords population
-    neighboringCoords = difference (nub (populationCoords >>= neighbors)) populationCoords
-    nextStateInNeighboringCoords coords = Cell coords (stateChangeConfig (Dead 0) (numberOfAliveNeighbors coords))
-    offspring = filter isAlive (map nextStateInNeighboringCoords neighboringCoords)
+    coordsOfAliveCells = (map getCoords (filter isAlive population)) :: Array Coords
+    numberOfAliveNeighbors coords = (length (intersect coordsOfAliveCells (neighbors coords))) :: Int
+    nextState (Cell coords state) = (map (\s -> Cell coords s) (stateChangeConfig state (numberOfAliveNeighbors coords))) :: Eff (random :: RANDOM) Cell
+    populationCoords = (map getCoords population) :: Array Coords
+    neighboringCoords = (difference (nub (populationCoords >>= neighbors)) populationCoords) :: Array Coords
+    nextStateInNeighboringCoords coords = (map (\s -> Cell coords s) (stateChangeConfig (Dead 0) (numberOfAliveNeighbors coords))) :: Eff (random :: RANDOM) Cell
+    neighboringCells = (sequence (map nextStateInNeighboringCoords neighboringCoords)) :: Eff (random :: RANDOM) (Array Cell)
+    offspring = (map (\ss -> filter isAlive ss) neighboringCells) :: Eff (random :: RANDOM) (Array Cell)
 
 neighbors :: Coords -> Array Coords
 neighbors (Coords { x, y }) = map (\d -> Coords { x: d.dx + x, y: d.dy + y }) directions
@@ -43,11 +49,11 @@ neighbors (Coords { x, y }) = map (\d -> Coords { x: d.dx + x, y: d.dy + y }) di
                     guard $ not (dx == 0 && dy == 0)
                     pure { dx, dy }
 
-stateChange :: CellState -> CellContext -> CellState
-stateChange Alive n | n == 2 || n == 3  = Alive
-                    | otherwise         = Dead 0
-stateChange (Dead since) n | since == 2 = Zombie -- TODO 30% chance
-                           | n == 3     = Alive
-                           | otherwise  = Dead (since+1)
-stateChange Zombie n | n > 3            = Dead 0
-                     | otherwise        = Zombie
+stateChange :: StateChangeConfig
+stateChange Alive n | n == 2 || n == 3  = pure Alive
+                    | otherwise         = pure (Dead 0)
+stateChange (Dead since) n | since == 2 = map (\r -> if r < 0.3 then Zombie else Dead (since+1)) (randomRange 0.0 1.0)
+                           | n == 3     = pure Alive
+                           | otherwise  = pure (Dead (since+1))
+stateChange Zombie n | n > 3            = pure (Dead 0)
+                     | otherwise        = pure Zombie
--- src/Main.purs
+++ src/Main.purs
@@ -10,13 +10,15 @@ import CSS.Geometry (left, top)
 import CSS.Size (px)
 import Control.Monad.Aff (Aff, later')
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random (RANDOM)
+import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Data.Int (toNumber)
 import Halogen.Util (awaitBody, runHalogenAff)
 import Prelude hiding (top)
 
 data Query a = Tick a
 
-type State = { cells :: Array L.Cell }
+type State = L.Population
 
 cell :: Int -> Int -> L.Cell
 cell x y = L.Cell (L.Coords { x: x, y: y }) L.Alive
@@ -27,17 +29,17 @@ toString (L.Dead _) = "dead"
 toString L.Zombie   = "zombie"
 
 initialState :: State
-initialState = { cells: [ cell 2 1, cell 3 2, cell 3 3, cell 2 3, cell 1 3 -- glider
-                        , cell 11 2, cell 12 2, cell 13 2 -- blinker
-                        , cell 2 11, cell 3 11, cell 2 12, cell 3 12 -- block
-                        , cell 31 20, cell 32 20, cell 30 21, cell 31 21, cell 31 22 -- r-pentomino
-                        ] }
+initialState = [ cell 2 1, cell 3 2, cell 3 3, cell 2 3, cell 1 3 -- glider
+               , cell 11 2, cell 12 2, cell 13 2 -- blinker
+               , cell 2 11, cell 3 11, cell 2 12, cell 3 12 -- block
+               , cell 31 20, cell 32 20, cell 30 21, cell 31 21, cell 31 22 -- r-pentomino
+               ]
 
 ui :: forall g. H.Component State Query g
 ui = H.component { render, eval }
   where
     render :: State -> H.ComponentHTML Query
-    render state = HH.div_ (map renderCell state.cells)
+    render state = HH.div_ (map renderCell state)
       where
         renderCell (L.Cell (L.Coords {x, y}) state) = HH.div [ HP.class_ $ HC.className $ toString state
                                                              , HS.style do left $ px $ toNumber $ 32 * x
@@ -46,10 +48,10 @@ ui = H.component { render, eval }
 
     eval :: Query ~> H.ComponentDSL State Query g
     eval (Tick next) = do
-      H.modify (\state -> { cells: L.gameOfLife state.cells })
+      H.modify (\state -> unsafePerformEff (L.gameOfLife state))  -- TODO unsafe
       pure next
 
-main :: Eff (H.HalogenEffects ()) Unit
+main :: Eff (H.HalogenEffects (random :: RANDOM)) Unit
 main = runHalogenAff do
   body <- awaitBody
   driver <- H.runUI ui initialState body
